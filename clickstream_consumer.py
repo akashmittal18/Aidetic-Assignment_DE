789c4c21060f21011ee45c2b3109431ae7314e56
Akash Mittal <akashmittal18@yahoo.com>
2023-07-21 22:44:40 +0530

    Update clickstream_consumer.py

--- clickstream_consumer.py
+++ clickstream_consumer.py
@@ -1,3 +1,7 @@
+'''
+    This code file contains code for Kafka consumer to consume data from the Kafka topics and store it in HBase.
+'''
+# Import necessary libraries
 from confluent_kafka import Consumer, KafkaError
 import happybase
 
@@ -17,15 +21,32 @@ table = hbase_connection.table('clickstream_table')
 while True:
     message = consumer.poll(1.0)
 
+    # Check if a message was received
     if message is None:
         continue
+
+    # Check for any errors while consuming the message
     if message.error():
         if message.error().code() == KafkaError._PARTITION_EOF:
+            # If the end of partition is reached, exit the loop
             print('Reached end of partition, exiting...')
         else:
+            # Print the error message
             print(f'Error while consuming: {message.error()}')
     else:
         # Process and store the clickstream data in HBase
         clickstream_data = message.value()
+
         # Extract relevant information from clickstream_data and store it in HBase using HBase client
-        table.put('unique_identifier', {'click_data:user_id': '123', 'click_data:timestamp': '2023-07-21 12:34:56', 'click_data:url': 'example.com', 'geo_data:country': 'USA', 'geo_data:city': 'New York', 'user_agent_data:browser': 'Chrome', 'user_agent_data:os': 'Windows', 'user_agent_data:device': 'Desktop'})
+        # Here, we are hardcoding some values for demonstration purposes, but in a real application,
+        # you would extract the required information from the clickstream_data.
+        table.put('unique_identifier', {
+            'click_data:user_id': '123',
+            'click_data:timestamp': '2023-07-21 12:34:56',
+            'click_data:url': 'example.com',
+            'geo_data:country': 'USA',
+            'geo_data:city': 'New York',
+            'user_agent_data:browser': 'Chrome',
+            'user_agent_data:os': 'Windows',
+            'user_agent_data:device': 'Desktop'
+        })
